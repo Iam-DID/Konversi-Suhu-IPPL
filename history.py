8ca91284a22433e190efccfc982f3e66e9b00d12
Darrel-jpg <darrelfa19@gmail.com>
2026-06-04 14:53:50 +0700

    commit

--- history.py
+++ history.py
@@ -5,7 +5,7 @@ def add_to_history(value, from_unit, result, to_unit):
     entry = f"{value} {from_unit} = {result} {to_unit}"
     _history_list.append(entry)
     
-    if len(_history_list) > 5:
+    if len(_history_list) > 10:
         _history_list.pop(0)
     return entry
 
